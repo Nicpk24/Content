1c81dfca80f42b47612cc08a15c4e54218e8b8fc
Nicpk24 <nickelly24@gmail.com>
2022-05-10 08:50:00 -0600

    message

---
+++ a1.0.0.0.0.2/src/NewApp.js
@@ -0,0 +1,16 @@
+import './NewApp.css';
+
+function NewApp() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome</h1>
+      </header>
+      <div id="1">
+        <h1>t</h1>
+      </div>
+    </div>
+  );
+}
+
+export default NewApp;
---
+++ s1.0.0.0.0.1/script.py
@@ -0,0 +1,11 @@
+function 1 (
+
+//todo cypher
+
+)
+
+function 2(
+
+//todo !uncypher
+
+)
